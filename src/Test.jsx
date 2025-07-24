253837b2c44f4550ed60da9d0e4c584ae47ffdbc
rohan911438 <rohan911438@example.com>
2025-07-25 00:01:58 +0530

    Updated About and Home components

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div style={{ color: 'white', padding: '20px', background: 'red' }}>
+      <h1>TEST COMPONENT WORKING!</h1>
+      <p>If you can see this, React is working properly.</p>
+    </div>
+  );
+};
+
+export default Test;
